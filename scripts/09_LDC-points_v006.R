a60b862319c71c3db573ff9e66728eda737f7aa6
Lia Ossanna <lossanna@arizona.edu>
2026-04-01 15:45:43 -0600

    Change v006 so it's not just MLRA with overlapping treatment polygons, but with actual treated LDC points within MLRA

--- scripts/09_LDC-points_v006.R
+++ scripts/09_LDC-points_v006.R
@@ -1,8 +1,8 @@
 # Created: 2026-04-01
 # Updated: 2026-04-01
 
-# Purpose: Create LDC points v006, which only includes control points in MLRAs that also
-#   have overlap with a treatment polygon.
+# Purpose: Create LDC points v006, which only includes control points in MLRAs that
+#   also contain treated points.
 
 
 library(tidyverse)
@@ -39,9 +39,25 @@ mlra.ctrl.inspect <- ldc.005.raw %>%
 unique(mlra.ctrl.inspect$Category)
 
 
+# Compare MLRA list vs. one based just on LDC location (not the pairwise intersect)
+mlra.ldc.005 <- ldc.005.raw %>% 
+  filter(!Category %in% c("Control, never burned", "Control, post-burn")) %>% 
+  select(MLRA_name) %>% 
+  distinct(.keep_all = TRUE)
+
+setdiff(mlra.005$MLRA_NAME, mlra.ldc.005$MLRA_name) # Cascade Mountains, Eastern Slope
+#   I checked on the map and there are only six points that fall in this MLRA; they are
+#     all very close to the border with another MLRA that has a lot of points (Malheur High 
+#     Plateau. There is just a tiny bit of overlap with an aerial seeding in the
+#     Cascade Mountains E Slope MLRA.
+#   It is probably easiest just to get rid of these points, because I'm not sure how
+#     they will be matched with propensity score matching (I think I would have to manually
+#     change the MLRA classification, and I don't to do that).
+
+
 # Filter to include LDC points only in relevant MLRAs
 ldc.006 <- ldc.005.raw %>% 
-  filter(MLRA_name %in% mlra.005$MLRA_NAME)
+  filter(MLRA_name %in% mlra.ldc.005$MLRA_name)
 
 
 # Write to CSV ------------------------------------------------------------
