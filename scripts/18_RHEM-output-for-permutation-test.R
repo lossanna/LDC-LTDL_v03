e5f37d2bb3438d522b9c541e4408e5f87338cb36
Lia Ossanna <lossanna@arizona.edu>
2026-06-01 16:37:21 -0600

    Begin script to compile RHEM output for permutation tests

---
+++ scripts/18_RHEM-output-for-permutation-test.R
@@ -0,0 +1,137 @@
+# Updated: 2026-06-01
+# Updated: 2026-06-01
+
+# Purpose: Format/gather RHEM data from geoindicators.csv for permutation test.
+
+library(tidyverse)
+
+# Load data ---------------------------------------------------------------
+
+load("RData/13_matched-data.RData")
+geoindicators.raw <- read_csv("data/raw/downloaded/ldc-data-2026-06-01/all/geoindicators.csv")
+
+
+# Format geoindicators data -----------------------------------------------
+
+# Adjust column names of geoindicators
+col_rename_map <- c(
+  "Project Key" = "ProjectKey",
+  "Primary Key" = "PrimaryKey",
+  "Date Visited" = "DateVisited",
+  "Ecological Site ID" = "EcoSiteID",
+  "Latitude (decimal degrees, NAD83)" = "Latitude",
+  "Longitude (decimal degrees, NAD83)" = "Longitude",
+  "Location Status" = "LocationStatus",
+  "Location Type" = "LocationType",
+  "Latitude, Actual (decimal degrees, NAD83)" = "LatActual",
+  "Longitude, Actual (decimal degrees, NAD83)" = "LonActual",
+  "Bare Soil (% First Hit)" = "BareSoil_FH",
+  "Total Foliar Cover (%)" = "TotalFoliarCover",
+  "Annual Forb Cover (% Any Hit)" = "AnnForbCover_AH",
+  "Annual Graminoid Cover (% Any Hit)" = "AnnGramCover_AH",
+  "Forb Cover (% Any Hit)" = "ForbCover_AH",
+  "Annual Forb and Graminoid Cover (% Any Hit)" = "AnnForbGramCover_AH",
+  "Graminoid Cover (% Any Hit)" = "GramCover_AH",
+  "Perennial Forb Cover (% Any Hit)" = "PerForbCover_AH",
+  "Perennial Forb and Graminoid Cover (% Any Hit)" = "PerForbGramCover_AH",
+  "Perennial Graminoid Cover (% Any Hit)" = "PerGramCover_AH",
+  "Shrub Cover (% Any Hit)" = "ShrubCover_AH",
+  "FH Cyanobacteria Cover (% First Hit)" = "CyanobacteriaCover_FH",
+  "Deposited Soil Cover (% First Hit)" = "DepositedSoilCover_FH",
+  "Duff Cover (% First Hit)" = "DuffCover_FH",
+  "Embedded Litter Cover (% First Hit)" = "EmbeddedLitterCover_FH",
+  "Herbaceous Litter Cover (% First Hit)" = "HerbLitterCover_FH",
+  "Lichen Cover (% First Hit)" = "LichenCover_FH",
+  "Moss Cover (% First Hit)" = "MossCover_FH",
+  "Rock Cover (% First Hit)" = "RockCover_FH",
+  "Total Litter Cover (% First Hit)" = "TotalLitterCover_FH",
+  "Vagrant Lichen Cover (% First Hit)" = "VagrantLichenCover_FH",
+  "Water Cover (% First Hit)" = "WaterCover_FH",
+  "Woody Litter Cover (% First Hit)" = "WoodyLitterCover_FH",
+  "Canopy Gaps 25 - 50 cm (%)" = "Gap25_50",
+  "Canopy Gaps 51-100 cm (%)" = "Gap51_100",
+  "Canopy Gaps 101 - 200 cm (%)" = "Gap101_200",
+  "Canopy Gaps > 200 cm (%)" = "Gap200plus",
+  "Canopy Gaps > 25 cm (%)" = "Gap25plus",
+  "Mean Forb Height (cm)" = "MeanForbHgt",
+  "Mean Graminoid Height (cm)" = "MeanGramHgt",
+  "Mean Herbaceous Plant Height (cm)" = "MeanHerbHgt",
+  "Mean Perennial Forb Height (cm)" = "MeanPerForbHgt",
+  "Mean Perennial Forb Graminoid Height (cm)" = "MeanPFbGrHgt",
+  "Mean Perennial Graminoid Height (cm)" = "MeanPerGramHgt",
+  "Mean Woody Plant Height (cm)" = "MeanWoodyHgt",
+  "Total Annual Production (Rangeland Health)" = "TotAnnualProduction_RH",
+  "Bare Ground (Rangeland Health)" = "BareGround_RH",
+  "Biotic Integrity (Rangeland Health)" = "BioticIntegrity_RH",
+  "Comments: Biotic Integrity (Rangeland Health)" = "BioticIntegrity_comments",
+  "Comments: Hydrologic Function (Rangeland Health)" = "HydrologicFunction_comments",
+  "Comments: Soil and Site Stability (Rangeland Health)" = "SoilSiteStability_comments",
+  "Compaction (Rangeland Health)" = "Compaction_RH",
+  "Proportion of Dead or Dying Plant Parts (Rangeland Health)" = "PropDeadDyingPlants_RH",
+  "Functional/Sructural Groups (Rangeland Health)" = "FunctionalStructuralGroups_RH",
+  "Gullies (Rangeland Health)" = "Gullies_RH",
+  "Hydrologic Function (Rangeland Health)" = "HydrologicFunction_RH",
+  "Invasive Plants (Rangeland Health)" = "InvasivePlants_RH",
+  "Litter Amount (Rangeland Health)" = "LitterAmount_RH",
+  "Litter Movement (Rangeland Health)" = "LitterMovement_RH",
+  "Pedestals/Terracettes (Rangeland Health)" = "Pedestals_RH",
+  "Plant Community Composition (Rangeland Health)" = "PlantCommunityComposition_RH",
+  "Perennial Reproductive Capability (Rangeland Health)" = "PerReproCapactiy_RH",
+  "Rills (Rangeland Health)" = "Rills_RH",
+  "Soil Site Stability (Rangeland Health)" = "SoilSiteStability_RH",
+  "Soil Surface Loss/Degradation (Rangeland Health)" = "SoilSurfaceLoss_RH",
+  "Soil Surface Erosion Resistance (Rangeland Health)" = "SoilErosionResistance_RH",
+  "Water Flow Patterns (Rangeland Health)" = "WaterFlowPatterns_RH",
+  "Wind Scoured Areas (Rangeland Health)" = "WindScouredAreas_RH",
+  "Mean Soil Stability: Surface" = "MeanSoilStability_Surface",
+  "Mean Soil Stability: Protected Samples" = "MeanSoilStability_Protected",
+  "Mean Soil Stability: Unprotected Samples" = "MeanSoilStabilityUnprotected",
+  "MLRA Description" = "MLRADesc_LDC",
+  "MLRA Symbol" = "MLRASym_LDC",
+  "Ecoregion Level I" = "EcoLvl1_LDC",
+  "Ecoregion Level II" = "EcoLvl2_LDC",
+  "Ecoregion Level III" = "EcoLvl3_LDC",
+  "Ecoregion Level IV" = "EcoLvl4_LDC",
+  "State" = "State",
+  "MODIS IGBP Name" = "MODISName",
+  "Database Key" = "DBKey",
+  "Date Loaded in Database" = "DateLoad",
+  "Total Horizontal Flux" = "TotalHorizontalFlux",
+  "Total Vertical Flux" = "TotalVerticalFlux",
+  "PM 2.5 Vertical Flux" = "PM25Flux",
+  "PM 10 Vertical Flux" = "PM10Flux",
+  "Long-Term Mean Precipitation" = "LongTermMeanPrecip",
+  "Long-Term Mean Runoff" = "LongTermMeanRunoff",
+  "Long-Term Mean Sediment Yield" = "LongTermMeanSedimentYield",
+  "Long-Term Mean Soil Loss" = "LongTermMeanSoilLoss"
+)
+
+geoindicators <- geoindicators.raw |>
+  rename(!!!setNames(names(col_rename_map), col_rename_map))
+
+# Identify completely empty columns
+empty_cols <- geoindicators |>
+  summarise(across(everything(), ~ all(is.na(.)))) |>
+  pivot_longer(everything(), names_to = "column", values_to = "is_empty") |>
+  filter(is_empty) |>
+  pull(column)
+
+geoindicators <- geoindicators |>
+  select(-all_of(empty_cols))
+
+
+
+# All matched data --------------------------------------------------------
+
+all.matched <- mget(ls(pattern = "\\.matched$")) |>
+  bind_rows(.id = "Model") |> 
+  select(PrimaryKey, Model)
+all.matched$Model <- sub("\\..*", "", all.matched$Model)
+
+
+# Combine -----------------------------------------------------------------
+
+rhem <- geoindicators |> 
+  filter(PrimaryKey %in% all.matched$PrimaryKey) |> 
+  select(PrimaryKey, LongTermMeanRunoff, LongTermMeanSoilLoss)
+  
